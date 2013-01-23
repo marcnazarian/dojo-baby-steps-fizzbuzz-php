a5f9fc7f6a1605d63a2749a174a9f419f34db13c
Marc Nazarian <mnazarian@bestofmedia.com>
2013-01-23 13:11:14 +0100

    Complete fizz buzz list up to 31 (and I will stop here ;-)

--- test/FizzBuzzTest.php
+++ test/FizzBuzzTest.php
@@ -45,8 +45,12 @@ class FizzBuzzTest extends PHPUnit_Framework_TestCase {
   
   /** @test */
   public function getFizzBuzzList() {
-    $fizzBuzzList = array(1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz', 16, 17, 'Fizz', 19, 'Buzz', 'Fizz', 22);
-    $this->assertEquals($fizzBuzzList, $this->fizzBuzz->getFizzBuzzList(22));
+    $fizzBuzzList = array(
+        1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 
+        11, 'Fizz', 13, 14, 'FizzBuzz', 16, 17, 'Fizz', 19, 'Buzz', 
+        'Fizz', 22, 23, 'Fizz', 'Buzz', 26, 'Fizz', 28, 29, 'FizzBuzz',
+        31);
+    $this->assertEquals($fizzBuzzList, $this->fizzBuzz->getFizzBuzzList(31));
   }
   
 
