603096d50abea10fcf79701dd93b75200239cc40
Marc Nazarian <mnazarian@bestofmedia.com>
2013-01-23 12:46:19 +0100

    Refacto: added setup function in test class

--- test/FizzBuzzTest.php
+++ test/FizzBuzzTest.php
@@ -4,16 +4,18 @@ require(__DIR__.'/../src/FizzBuzz.php');
 
 class FizzBuzzTest extends PHPUnit_Framework_TestCase {
   
+  public function setUp() {
+    $this->fizzBuzz = new FizzBuzz();
+  }
+  
   /** @test */
   public function number1returns1() {
-    $fizzBuzz = new FizzBuzz();
-    $this->assertEquals('1', $fizzBuzz->displayFizzBuzz(1));
+    $this->assertEquals('1', $this->fizzBuzz->displayFizzBuzz(1));
   }
   
   /** @test */
   public function number3returnsFizz() {
-    $fizzBuzz = new FizzBuzz();
-    $this->assertEquals('Fizz', $fizzBuzz->displayFizzBuzz(3));
+    $this->assertEquals('Fizz', $this->fizzBuzz->displayFizzBuzz(3));
   }
 
 }
